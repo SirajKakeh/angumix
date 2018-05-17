938276c27d368c53bb981690b931f342714ccefb
SirajKakeh <sirajonline.89@gmail.com>
2018-05-17 22:43:09 +0300

    init

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-/**
- * AngularJS Tutorial 1
- * @author Nick Kaye <nick.c.kaye@gmail.com>
- */
-
-/**
- * Main AngularJS Web Application
- */
 var app = angular.module('tutorialWebApp', [
   'ngRoute'
 ]);
